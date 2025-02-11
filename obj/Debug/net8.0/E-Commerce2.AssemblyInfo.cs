150a6d641d3fbbcdaca492831d712cba8846a73d
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-02-11 02:53:28 +0200

    Product detail update

--- obj/Debug/net8.0/E-Commerce2.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-Commerce2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed4a9f414e578836612c0c7971cb00178961b73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2121ef1c98e2d13867c27b5fc94113cfd9f9b053")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
