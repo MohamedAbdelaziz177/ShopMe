d869689f8489ed5f10892af0b1dbace2a202dde7
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-02-18 03:07:30 +0200

    configuring Stripe

--- obj/Debug/net8.0/E-Commerce2.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-Commerce2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+757c7d63fcc1a58beb15e848f35493521719186c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640f4a5b1faf863314b66639472b1d87e1c3d0e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
